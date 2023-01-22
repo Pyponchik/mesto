aeac852c7476448f56912b80b171b306a49a54c7
Evgeny Azarkin <e.e.azarkin@gmail.com>
2023-01-22 19:53:06 +0500

    второе ревью

--- scripts/index.js
+++ scripts/index.js
@@ -6,19 +6,21 @@ let job = document.querySelector('.profile__job');
 let formElement = document.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
 let nameInput =  document.querySelector('.popup__input_type_name');// Воспользуйтесь инструментом .querySelector()
 let jobInput =  document.querySelector('.popup__input_type_job');// Воспользуйтесь инструментом .querySelector()
-popupIconOpen.addEventListener('click', function(){
-  popup.classList.add('popup_opened');
-  nameInput.value = name.textContent;
-  jobInput.value = job.textContent;
-})
-popupIconClose.addEventListener('click', popupClose);
-function popupClose(){
-  popup.classList.remove('popup_opened');
-}
+
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
     name.textContent = nameInput.value;
     job.textContent = jobInput.value;
     popupClose();
 }
+
 formElement.addEventListener('submit', handleFormSubmit);
+popupIconClose.addEventListener('click', popupClose);
+function popupClose(){
+  popup.classList.remove('popup_opened');
+}
+popupIconOpen.addEventListener('click', function(){
+  popup.classList.add('popup_opened');
+  nameInput.value = name.textContent;
+  jobInput.value = job.textContent;
+})
