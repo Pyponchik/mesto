ece84246a7a4f0a25730a7c5f678e1f517350330
Evgeny Azarkin <e.e.azarkin@gmail.com>
2023-02-07 01:11:24 +0500

    5 пр 2 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -4,39 +4,46 @@ const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 const nameInput =  popupEditProfile.querySelector('.popup__input_type_name');
 const jobInput =  popupEditProfile.querySelector('.popup__input_type_job');
-document.querySelector('.profile__editButton').addEventListener('click', ()=>{
-  popupOpenClose(popupEditProfile);
+const profileEditButton = document.querySelector('.profile__editButton');
+const popupEditProfileForm = popupEditProfile.querySelector('.popup__form');
+const popupEditProfileCloseIcon= popupEditProfile.querySelector('.popup__closeIcon');
+profileEditButton.addEventListener('click', ()=>{
+  togglePopup(popupEditProfile);
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 });
-popupEditProfile.querySelector('.popup__form').addEventListener('submit', function(evt){
+popupEditProfileForm.addEventListener('submit', function(evt){
   evt.preventDefault(); 
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popupOpenClose(popupEditProfile);
+  togglePopup(popupEditProfile);
 });
-popupEditProfile.querySelector('.popup__closeIcon').addEventListener('click', ()=>{
-  popupOpenClose(popupEditProfile);
+popupEditProfileCloseIcon.addEventListener('click', ()=>{
+  togglePopup(popupEditProfile);
 });///////открытие 2 попапа///////////
-
 const popupAddCard = document.querySelector('.popup_add-card');
 const popupAddCardForm = popupAddCard.querySelector('.popup__form');
-document.querySelector('.profile__addButton').addEventListener('click',()=>{
-popupOpenClose(popupAddCard);
+const profileAddButton = document.querySelector('.profile__addButton');
+const popupAddCardInputName = popupAddCardForm.querySelector('.popup__input_name')
+const popupAddCardInputLink = popupAddCardForm.querySelector('.popup__input_link')
+const popupAddCardCloseIcon = popupAddCard.querySelector('.popup__closeIcon');
+profileAddButton.addEventListener('click',()=>{
+  togglePopup(popupAddCard);
 });
 popupAddCardForm.addEventListener('submit', function(evt){
   evt.preventDefault(); 
   addCard(newElement = 
-    {name: `${popupAddCardForm.querySelector('.popup__input_name').value}`,
-    link: `${popupAddCardForm.querySelector('.popup__input_link').value}`
+    {name: `${popupAddCardInputName.value}`,
+    link: `${popupAddCardInputLink.value}`
   });
   popupAddCardForm.reset();
-  popupOpenClose(popupAddCard);
+  togglePopup(popupAddCard);
 });
-popupAddCard.querySelector('.popup__closeIcon').addEventListener('click', ()=>{
-  popupOpenClose(popupAddCard);
+popupAddCardCloseIcon.addEventListener('click', ()=>{//////////////////////////////////////////////////////////////
+  togglePopup(popupAddCard);
 });//////добавление карточек,лайки и удаление//////
 const elements = document.querySelector('.elements');
+const popupImage = document.querySelector('.popupImage');
 const initialCards = [
   {
     name: 'Архыз',
@@ -66,38 +73,38 @@ const initialCards = [
 initialCards.forEach((arr) => {
   addCard(arr);
 });
-function createCard(array){
+function createCard(cardData){
   const card = document.querySelector('.template').content.querySelector('.element').cloneNode(true);
   card.querySelector('.element__trash').addEventListener('click', ()=>{
     card.remove();
   });
   const elementImage = card.querySelector('.element__image');
-  const popupImage = document.querySelector('.popupImage')
   const popupImageImage = popupImage.querySelector('.popupImage__image');
-  card.querySelector('.element__image').addEventListener('click',()=> {
-    popupImageImage.alt = array.name;
-    popupImageImage.src = array.link;
-    popupImage.querySelector('.popupImage__name').textContent = array.name;
-    popupOpenClose(popupImage);
-  });
-  popupImage.querySelector('.popupImage__closeIcon').addEventListener('click', ()=>{
-    popupImage.classList.remove('popup_opened');
+  const popupImageName = popupImage.querySelector('.popupImage__name');
+  elementImage.addEventListener('click',()=> {
+    popupImageImage.alt = cardData.name;
+    popupImageImage.src = cardData.link;
+    popupImageName.textContent = cardData.name;
+    togglePopup(popupImage);
   });
   const elementLike = card.querySelector('.element__like');
   elementLike.addEventListener('click', ()=> {
     elementLike.classList.toggle('element__like_active');
   });
-  card.querySelector('.element__name').textContent = array.name;
-  elementImage.src = array.link;
-  elementImage.alt = array.name;
+  card.querySelector('.element__name').textContent = cardData.name;
+  elementImage.src = cardData.link;
+  elementImage.alt = cardData.name;
   elementImage.onerror = function(){
     elementImage.src = 'https://торги-россии.рф/pictures/thumbs/$2y$10$i5aegDuGn.RhbiwYvDsQVeh1lg5xqlRNn8FYyNrDHdmpUmBbeTG.jpeg';
   }
   return card;
 }
-function addCard(array){
-  elements.prepend(createCard(array));
+document.querySelector('.popupImage__closeIcon').addEventListener('click', ()=>{////////////////////////////////////////////////////////////////
+  togglePopup(popupImage);
+});
+function addCard(cardData){
+  elements.prepend(createCard(cardData));
 }///////открытие-закрытие попапа/////////////
-function popupOpenClose(tipe){
-  tipe.classList.toggle('popup_opened');
+function togglePopup(popup){
+  popup.classList.toggle('popup_opened');
 }
